9395416624acec025c7c7862f071ed998136658d
Kalo <kalolaini.tukana@gmail.com>
2026-05-11 14:15:10 +1000

    Configure Flask app for Render deployment

--- Backend/app.py
+++ Backend/app.py
@@ -451,4 +451,5 @@ def clear_session():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, threaded=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port, threaded=True)
